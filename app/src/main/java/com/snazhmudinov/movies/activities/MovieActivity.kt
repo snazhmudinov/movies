53227698f4c95ce8d08424d9d8945f65c71075fe
snazhmudinov <snazhmudinov@gmail.com>
2017-06-10 12:35:06 +0200

    extracting the movie in the second activity

--- app/src/main/java/com/snazhmudinov/movies/activities/MovieActivity.kt
+++ app/src/main/java/com/snazhmudinov/movies/activities/MovieActivity.kt
@@ -3,7 +3,10 @@ package com.snazhmudinov.movies.activities
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.snazhmudinov.movies.R
+import com.snazhmudinov.movies.constans.Constants
+import com.snazhmudinov.movies.models.Movie
 import kotlinx.android.synthetic.main.activity_movie.*
+import org.parceler.Parcels
 
 /**
  * Created by snazhmudinov on 6/10/17.
@@ -15,6 +18,14 @@ class MovieActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie)
 
+        val movie : Movie = Parcels.unwrap(intent.getParcelableExtra(Constants.MOVIE_KEY))
+
+        configureToolbar()
+
+        movie_toolbar.title = movie.originalTitle
+    }
+
+    fun configureToolbar() {
         setSupportActionBar(movie_toolbar)
         movie_toolbar.setNavigationOnClickListener {
             finish()

7c51ca3a22415cc0a65641a286b562d140a5512f
snazhmudinov <snazhmudinov@gmail.com>
2018-02-04 14:10:18 +0100

    close only if in landscape mode && it's a tablet

--- app/src/main/java/com/snazhmudinov/movies/activities/MovieActivity.kt
+++ app/src/main/java/com/snazhmudinov/movies/activities/MovieActivity.kt
@@ -8,6 +8,7 @@ import com.snazhmudinov.movies.R
 /**
  * Created by snazhmudinov on 6/10/17.
  */
+private const val SMALLEST_WIDTH_TABLET = 600
 
 class MovieActivity : AppCompatActivity() {
 
@@ -19,7 +20,8 @@ class MovieActivity : AppCompatActivity() {
     override fun onConfigurationChanged(newConfig: Configuration?) {
         super.onConfigurationChanged(newConfig)
         newConfig?.let {
-            if (it.orientation == Configuration.ORIENTATION_LANDSCAPE) { finish() }
+            if (it.orientation == Configuration.ORIENTATION_LANDSCAPE &&
+                    it.smallestScreenWidthDp >= SMALLEST_WIDTH_TABLET) { finish() }
         }
     }
 }
